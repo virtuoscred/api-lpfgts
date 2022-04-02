a335990433089450e02fba33051defb7da8e4137
virtuoscred <102836128+virtuoscred@users.noreply.github.com>
2022-04-01 21:47:29 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 3001
 import Routes from './router'
 import cors from 'cors'
 
-app.use(cors({
-    origin: process.env.URL_ORIGIN
-}))
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
